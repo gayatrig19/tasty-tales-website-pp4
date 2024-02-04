898fea33923d4fb54ae0b845458895369be5b4fb
Gayatri Ghogare <ghogaregayatri@gmail.com>
2024-02-04 22:26:17 +0000

    Setup templates for base.html and basic structurefor app

--- tastytales/urls.py
+++ tastytales/urls.py
@@ -16,10 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from blog import views as index_views
 
 
 urlpatterns = [
-    path('', index_views.index, name='index'),
     path('admin/', admin.site.urls),
+    path('', include('blog.urls')),
 ]
