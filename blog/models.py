8f14165a4119917e9fa3c098612a569a880c1024
Gayatri Ghogare <ghogaregayatri@gmail.com>
2024-02-10 21:42:13 +0000

    Create a Recipe Model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+
+
+
+STATUS = ((0, "Draft"), (1, "Published"))
+
 
 # Create your models here.
+class Recipe(models.Model):
+
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='recipe_owner')
+    title = models.CharField(max_length=200, unique=True)
+    description = models.CharField(max_length=500, null=False, blank=False)
+    featured_image = CloudinaryField('image', default='placeholder')
+    ingredients = models.TextField(blank=False)
+    instructions = models.TextField(blank=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    likes = models.ManyToManyField(User, default=None, blank=True,
+                                   related_name='recipe_likes')
+    
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return f'{self.title} | written by {self.author}'
+
+    def total_num_of_likes(self):
+        return self.likes.count()
+    
+
+
+
+
