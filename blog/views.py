8d5835326cced0303d1e766b3882fd63b0e46a76
Gayatri Ghogare <ghogaregayatri@gmail.com>
2024-02-25 15:43:53 +0000

    Tidy up views.py for order of imports and comments

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, reverse
-from django.views import generic, View
 from django.http import HttpResponseRedirect
+from django.urls import reverse_lazy
+from django.views import generic, View
 from django.contrib.auth.mixins import UserPassesTestMixin, LoginRequiredMixin
 from django.views.generic import (
     TemplateView, CreateView, ListView, DeleteView, UpdateView)
@@ -8,19 +9,23 @@ from django.db.models import Q
 from django.contrib import messages
 from .models import Recipe, Comment
 from .forms import RecipeForm, CommentForm
-from django.urls import reverse_lazy
 
 
 class IndexView(TemplateView):
     """
-    View to display home page
+    View to display home page.
     """
     template_name = "blog/index.html"
 
 
 class RecipeList(ListView):
     """
-    Class based view to display list of recipes
+    Class based view to display list of published recipes.
+    Displays 6 recipes per page, filtered to show only
+    recipes with a status of 'published',
+    The recipes are ordered by the date they are created,
+    with the latest ones appearing first.
+
     """
     template_name = "blog/recipes.html"
     model = Recipe
@@ -28,12 +33,20 @@ class RecipeList(ListView):
     paginate_by = 6
 
     def get_queryset(self):
+        """
+        Retrieve the queryset of published recipes ordered
+        by created date.
+        """
         return Recipe.objects.filter(status=1).order_by('-created_on')
 
 
 class RecipeDetail(View):
     """
-    View to render recipe detail
+    View to render recipe in detail.
+    Allows logged-in users to leave comment.
+    Retrieves info about comments and likes
+    Handles comment form submission when valid and displays
+    success message to user.
     """
     def get(self, request, slug, *args, **kwargs):
         queryset = Recipe.objects.filter(status=1)
@@ -92,13 +105,17 @@ class RecipeDetail(View):
 
 class AddRecipe(LoginRequiredMixin, CreateView):
     """
-    View to add/create recipes
+    Class based view to add/create recipes
+    Requires user to be logged in.
+    On successful form submission, redirects to the 'recipes' page.
+    If the form is valid, sets the author of the recipe
+    to the current logged-in user.
+    Displays a success message to the user.
     """
     model = Recipe
     form_class = RecipeForm
     template_name = "blog/add_recipe.html"
     success_url = reverse_lazy('recipes')
-
 
     # Source: https://stackoverflow.com/questions/67366138/django-display-message-after-creating-a-post #noqa
     def form_valid(self, form):
@@ -110,13 +127,17 @@ class AddRecipe(LoginRequiredMixin, CreateView):
 
 class UpdateRecipe(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     """
-    View to edit recipe
+    Class based view to edit/update recipe
+    Requires the user to be logged in and be the author of the recipe.
+    On successful form submission, redirects to the 'recipes' page.
+    Handles the form submission when valid.
+    Displays a success message to the user
+    on successful update.
     """
     model = Recipe
     form_class = RecipeForm
     template_name = "blog/update_recipe.html"
     success_url = reverse_lazy('recipes')
-
 
     def test_func(self):
         return self.request.user == self.get_object().author
@@ -130,7 +151,10 @@ class UpdateRecipe(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     
 class DeleteRecipe(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     """
-    View to delete recipe
+    Class based view to delete recipe.
+    Checks if the user is authorised to delete recipe.
+    Manages delete request and override form_valid to
+    displays a success message to user after successful deletion.
     """
     model = Recipe
     success_url = reverse_lazy('recipes')
@@ -145,6 +169,13 @@ class DeleteRecipe(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
 
 
 class RecipeLike(View):
+    """
+    View to handle like/unlike recipe.
+    Allows only logged-in users to like/unlike the recipe.
+    Checks if the user has liked/unliked the recipe.
+    Adds like if not/ removes like if yes
+    Redirects user back to same page.
+    """
 
     def post(self, request, slug):
         recipe = get_object_or_404(Recipe, slug=slug)
@@ -152,14 +183,17 @@ class RecipeLike(View):
         if recipe.likes.filter(id=request.user.id).exists():
             recipe.likes.remove(request.user)
         else:
-            recipe.likes.add(request.user)
-        
+            recipe.likes.add(request.user) 
         return HttpResponseRedirect(reverse('recipe_detail', args=[slug]))
 
 
 class UserDrafts(ListView):
     """
-    Class based views to view users own recipes
+    Class based view to display user's draft recipes.
+    Displays drafts created by the currently logged-in user.
+    Only visible to the draft author.
+    Returns a list of recipes with status of 'draft'.
+    Displays 6 recipes per page.
     """
 
     template_name = "blog/my_drafts.html"
@@ -172,11 +206,16 @@ class UserDrafts(ListView):
        
         
 class RecipeSearchList(ListView):
+    """
+    View to display a list of recipes based on user search.
+    Searches for recipes based on title, cuisine type, and description.
+    Retrieves search for only recipes with status 'published'.
+    Displays 6 recipes per page.
+    """
     model = Recipe
     template_name = 'blog/recipe_search.html'
     context_object_name = 'recipes'
     paginate_by = 6
-
 
     def get_queryset(self):
         queryset = super().get_queryset()
@@ -189,7 +228,6 @@ class RecipeSearchList(ListView):
                 status=1  
             ).order_by('-created_on')
         else:
-            # If no search query is provided, return an empty queryset
             queryset = Recipe.objects.none()
         return queryset
 
